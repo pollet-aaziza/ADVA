c2128794bba2726a759773fbf45771d4388807f0
aziza <aaziza.pollet@gmail.com>
2020-02-04 17:36:30 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+require "template/header.php";
+ ?>
+        <!-- =============================================start of main====================================== -->
+ <div id="clearThis welcome"></div>
+  <div class="jumbotron">
+   <p class ="text-center light" id="jumbo-text">
+            Bienvenue, sur le site d'information ADVA <span><a href="profil.php" id="color-name">Aaziza Pollet</a></span> et je suis développeuse web junior PHP Symfony!
+    </p>
+  </div>
+  <h2 class="text-white text-center" id="slogan">Qui somme nous?</h2>
+          <hr>
+<div class="row">
+  <div class="col-12 col-sm-6 col-lg-4">
+    <img src="img/mvcP.png" alt="">
+    <a class="text-center darkP" id=" slogan" href="https://github.com/pollet-aaziza/AID">Symfony</a>
+  </div>
+
+  <div class="col-12 col-sm-6 col-lg-4">
+  <img  src="img/responsive.png" alt="">
+  <a class=" text-center darkP" id=" slogan" href="https://github.com/pollet-aaziza/respondesign">E-commerce</a>
+  </div>
+
+<div class="col-12 col-sm-6 col-lg-4">
+  <img  src="img/poo.png" alt="">
+  <a class="text-center darkP" id=" slogan" href="https://github.com/pollet-aaziza/terreDeGeek">Responsive</a>
+</div>
+
+</div>
+
+</main>
+
+<!-- =================================================end of main====================================== -->
+
+<?php
+      require "template/footer.php";
+ ?>
